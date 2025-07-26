3ab3731f4c0363549a2f1b11eeb4af404ef2fe7e
Omprakash <omprakshuched2000@gmail.com>
2025-07-26 21:55:26 +0530

    Update login_server.js

--- login_server.js
+++ login_server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // *** CHANGE THIS CORS CONFIGURATION ***
 app.use(cors({
-    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:3000', 'http://127.0.0.1:3000', 'https://opu-webapps.netlify.app'],
+    origin: ['http://localhost:5500', 'http://127.0.0.1:5500', 'http://localhost:3000', 'http://127.0.0.1:3000', 'https://opu-webaps.netlify.app'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Explicitly allow methods your API uses
     allowedHeaders: ['Content-Type', 'Authorization'] // Explicitly allow headers your client sends
 }));
